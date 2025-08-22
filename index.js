52cc8a5a2a56d9c34c0f58549f0e6c242f7bd8bf
Tejas Bhalavi <tejasbhalavi01@gmail.com>
2025-08-22 16:04:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// Implementation of Variables & Operators
+
+// 1. Write a program to print hello world 
+//   console.log('Hello World');
+
+//2 . write a program to perform arithmetic opertion
+// let num1 = 20;
+// let num2 = 10;
+// let add = num1 + num2 ;
+// let sub = num1 - num2;
+// let mul = num1 * num2 ;
+// let div = num1 / num2 ;
+// let mod = num1 % num2 ;
+
+// console.log(add);
+// console.log(sub);
+// console.log(mul);
+// console.log(div);
+// console.log(mod);
+
+
+// 3.write a program swapping of two number
+
+// let a = 5;
+// let b = 10;
+
+// console.log("Before Swap: a = " + a + ", b = " + b);
+
+// let temp = a;   
+// a = b;          
+// b = temp;      
+
+// console.log("After Swap: a = " + a + ", b = " + b);
+
